ebee87ccec9b216f634146e4f4320ddd88960a5f
Jon Sterling <jon@jons-macbook-pro.local>
2019-01-21 17:37:47 -0700

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by Jon Sterling on 1/21/19.
+//  Copyright © 2019 Jon Sterling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
